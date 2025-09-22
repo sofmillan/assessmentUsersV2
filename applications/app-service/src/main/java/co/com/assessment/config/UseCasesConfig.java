38eceb4985c046306aba802c3026b78d6fcbf530
Sofía Millán <sofiaisabellamillan16@gmail.com>
2025-09-22 16:45:53 -0500

    First commit

---
+++ applications/app-service/src/main/java/co/com/assessment/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.assessment.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.assessment.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
